0d3f39e1357f05b77c59b0f19822039afc39c50f
theFl00f <celeste.ellerby@gmail.com>
2020-02-12 20:08:33 -0500

    reset button functional, score displayed in dialogue

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,17 +5,58 @@ $(document).ready(() => {
     const $char = $('.char')
     const $main = $('main')
     const $score = $('span')
+    const $p = $('p')
+    let score = 0;
+
+
+    // if char borderbox intersects with obstacle's borderbox, execute stop input from user
+    const charPosition = $char.offset();
+    const charWidth = $char.width();
+    const charHeight = $char.height();
+    const obstaclePosition = $obstacle.offset();
+    
+    const char = {
+        y: charPosition.top,
+        x: charPosition.left,
+        width: charWidth,
+        height: charHeight
+    }
+    
+    const obstacle = {
+        y: obstaclePosition.top,
+        x: obstaclePosition.left,
+        width: charWidth,
+        height: charHeight,
+        collide: false
+    }
 
     // on click of "start" close dialogue
     $char.addClass('disabled');
 
+    const gameStart = () => {
+        if ($dialogue.hasClass('hiddenDialogue') === false) {
+            $main.css('filter', 'none')
+            setTimeout(function() {
+                $char.removeClass('disabled')
+            }, 200);
+            $obstacle.css({left:'100%'}).delay(1000)
+            .animate({left: '-10%'}, 4600, "linear", function() {
+                increaseScore()
+                gameStart();
+                //OTHER TWO DO, IDK WHAT'S GOIN ON
+                timer()
+            });
+        }
+    }
+
     $button.on('click', function(e) {
         e.preventDefault();
-        $dialogue.toggle(false);
-        $main.css('filter', 'none')
-        setTimeout(function() {
-            $char.removeClass('disabled')
-        }, 200)
+        score = 0;
+        $score.html(score)
+        obstacle.collide = false;
+        $dialogue.toggle("hiddenDialogue");
+        gameStart()
+
     })
 
     // also on click of "start" activate method to jump set amount on input
@@ -51,7 +92,6 @@ $(document).ready(() => {
     })
     $
     // when char's coordinates are across the obstacle (or past a certain point) increase score held in variable.
-    let score = 0;
     $score.html(score)
     const increaseScore = () => {
         score++;
@@ -62,50 +102,12 @@ $(document).ready(() => {
     
     // also on click of "start" start method on bg obstacle objects to move across screen at solid rate
 
-
-    //ISSUE - DOESN'T EXECUTE EACH TIME
-    //things i've tried: declaring obstacleLocationInterval as a let globally and moving the function into the animationend
-    //same but with a const = null
-    //the issue is the scope. It calls once when it's declared on pageload, but am not able o return obstacleLocationInterval in a function
-
     const obstacleLocationInterval = setInterval(function() {
         updateObstacleLocation()
     }, 100)
-    
-    console.log(obstacleLocationInterval)
 
-    $button.on('click', function obstacleGo() {
-        
-        $obstacle.css({left:'100%'}).delay(1000)
-        .animate({left: '-10%'}, 4600, "linear", function() {
-            increaseScore()
-            obstacleGo();
-            //OTHER TWO DO, IDK WHAT'S GOIN ON
-            obstacleLocationInterval
-        });
-    })
-    
-    
-    
-    
-    // if char borderbox intersects with obstacle's borderbox, execute stop input from user
-    const charPosition = $char.offset();
-    const charWidth = $char.width();
-    const charHeight = $char.height();
-    const obstaclePosition = $obstacle.offset();
-    
-    const char = {
-        y: charPosition.top,
-        x: charPosition.left,
-        width: charWidth,
-        height: charHeight
-    }
-    
-    const obstacle = {
-        y: obstaclePosition.top,
-        x: obstaclePosition.left,
-        width: charWidth,
-        height: charHeight
+    const timer = () => {
+        return obstacleLocationInterval
     }
     
     const checkCollision = () => {
@@ -114,15 +116,17 @@ $(document).ready(() => {
             char.x < obstacle.x + obstacle.width &&
             char.x + char.width > obstacle.x) {
                 console.log('collision')
+                obstacle.collide = true;
                 // also stop movement on all units
                 $char.stop( true, false ).animate({bottom: '20%'}, 500, function() {
                     $char.addClass('disabled')
                 })
                 $obstacle.stop()
-                clearInterval(obstacleLocationInterval)
                 $main.css("filter", "brightness(75%) contrast(133%)")
+                $p.html(`your score: ${score}`)
+                $button.html('try again!')
                 setTimeout(function() {
-                    $dialogue.toggle(true)
+                    $dialogue.toggle('hiddenDialogue')
                     $char.addClass('disabled')
                 }, 300)
             }
@@ -131,7 +135,11 @@ $(document).ready(() => {
     const updateObstacleLocation = () => {
         obstacle.x = $obstacle.offset().left
         obstacle.y = $obstacle.offset().top
-        checkCollision()
+        if (obstacle.collide === false) {
+            checkCollision()
+        } else {
+            console.log('start game')
+        }
     }
     
     const updateCharLocation = () => {
